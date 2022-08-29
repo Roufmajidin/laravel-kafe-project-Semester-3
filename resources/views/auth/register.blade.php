c3db8d8a292a030731c9f6b756253ed59be57b7d
Roufmajidin <roufmajidin777@gmail.com>
2022-08-29 21:51:21 +0700

    3 - auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,13 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">level</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="text" class="form-control" name="level" required autocomplete="level">
+                            </div>
+                        </div>
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
